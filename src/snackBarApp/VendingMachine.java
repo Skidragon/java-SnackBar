f082c047901efc9c566a3a32aca92c796da32841
Simon Davis <simon_freelance@hotmail.com>
2019-02-04 15:49:35 -0500

    Made VendingMachine class by requirements

---
+++ src/snackBarApp/VendingMachine.java
@@ -0,0 +1,21 @@
+package snackBarApp;
+
+public class VendingMachine {
+    private static int maxId = 0;
+    private int id = 0;
+    private String name;
+
+    public VendingMachine(String name) {
+        maxId++;
+        id = maxId;
+
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
