57044facbad034b33f39a73eeecbdb0fdb79dfcb
Simon Davis <simon_freelance@hotmail.com>
2019-02-04 16:55:27 -0500

    Changed return type for sellSnack and removed totalCost variable

--- src/snackBarApp/Snack.java
+++ src/snackBarApp/Snack.java
@@ -8,11 +8,9 @@ public class Snack {
     private double cost;
     private int machineId;
 
-    public Snack (String name, int quantity, double cost, int machineId) {
+    public Snack(String name, int quantity, double cost, int machineId) {
         maxId++;
         id = maxId;
-
-        totalCost += cost;
 
         this.name = name;
         this.quantity = quantity;
@@ -27,12 +25,17 @@ public class Snack {
     public int getQuantity() {
         return quantity;
     }
+
     public void addQuantity(int quantity) {
         this.quantity += quantity;
     }
-    public void decreaseQuantity(int quantity) {
+
+    public double sellSnack(int quantity) {
         this.quantity -= quantity;
+
+        return quantity * cost;
     }
+
     public double getTotalCost(int quantity) {
         return cost * quantity;
     }
